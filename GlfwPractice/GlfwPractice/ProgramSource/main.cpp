2cb18349bdb78a60725505409c07983871f9b40c
QuinnJMosher <Darkstaro59@gmail.com>
2015-02-03 14:03:39 -0800

    prepare for test program

---
+++ GlfwPractice/GlfwPractice/ProgramSource/main.cpp
@@ -0,0 +1,18 @@
+#include "QuinnFramework.h"
+
+int main() {
+	FrameworkInitalize();
+	OpenWindow(640, 480, "FrameworkProgram");
+	SetFontSize(7);
+	while (!FrameworkUpdate()) {
+		//clear screen
+		FrameworkClearScreen();
+		//update
+
+		//draw
+		DrawString("The quick brown fox jumped over the lazy dog.", 10.0f, 300.0f);
+	}
+	FrameworkShutdown();
+
+	return 0;
+}
